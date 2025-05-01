7a4786a4eda71038128b982be84d508468be7a08
intansiregar70 <intansiregar79@admin.sma.belajar.id>
2025-05-02 00:50:32 +0700

    Add files via upload

---
+++ dataSiswa.js
@@ -0,0 +1,596 @@
+const dataSiswa = [
+  {
+    "nisn": "72317507",
+    "nama": "Abi Rafdi Nasrullah",
+    "tempatLahir": "Kerpai",
+    "tanggalLahir": "2007-04-16"
+  },
+  {
+    "nisn": "76471051",
+    "nama": "Adelia Suriani",
+    "tempatLahir": "Sidomukti",
+    "tanggalLahir": "2007-06-16"
+  },
+  {
+    "nisn": "68882812",
+    "nama": "AMELDA BR SEMBIRING",
+    "tempatLahir": "GERAT",
+    "tanggalLahir": "2006-11-13"
+  },
+  {
+    "nisn": "85305437",
+    "nama": "Arief Yudria Kurniawan",
+    "tempatLahir": "Semarang",
+    "tanggalLahir": "2008-04-19"
+  },
+  {
+    "nisn": "63910062",
+    "nama": "BAITTI LASTARI BR SITEPU",
+    "tempatLahir": "AMAN DAMAI",
+    "tanggalLahir": "2006-12-08"
+  },
+  {
+    "nisn": "72589381",
+    "nama": "CAHAYA TRIA MADHANI",
+    "tempatLahir": "Dumai",
+    "tanggalLahir": "2007-09-21"
+  },
+  {
+    "nisn": "72430214",
+    "nama": "DAVID WAHYU RAMADHANI",
+    "tempatLahir": "PADANG BRAHRANG",
+    "tanggalLahir": "2007-12-21"
+  },
+  {
+    "nisn": "64966185",
+    "nama": "DEA NABILA SARI",
+    "tempatLahir": "DUSUN SUKA MAJU",
+    "tanggalLahir": "2006-06-09"
+  },
+  {
+    "nisn": "79472686",
+    "nama": "DEVI NOVITA",
+    "tempatLahir": "BATU AMPAR",
+    "tanggalLahir": "2007-11-23"
+  },
+  {
+    "nisn": "76447730",
+    "nama": "DIMAS ADITYA",
+    "tempatLahir": "TANDEM",
+    "tanggalLahir": "2007-10-03"
+  },
+  {
+    "nisn": "79361013",
+    "nama": "DINA AULIYA",
+    "tempatLahir": "PAYA JAMBU ",
+    "tanggalLahir": "2007-08-05"
+  },
+  {
+    "nisn": "78620210",
+    "nama": "Dini Aulia Ilri Manurung",
+    "tempatLahir": "Dusun Afdeling III",
+    "tanggalLahir": "2007-11-30"
+  },
+  {
+    "nisn": "75708588",
+    "nama": "ELSYA AMELIA PUTRI",
+    "tempatLahir": "SIDODADI",
+    "tanggalLahir": "2007-08-28"
+  },
+  {
+    "nisn": "86505750",
+    "nama": "Febrin Aini Harahap",
+    "tempatLahir": "Sumber Rejo",
+    "tanggalLahir": "2008-02-11"
+  },
+  {
+    "nisn": "77668268",
+    "nama": "Firda Sapitri",
+    "tempatLahir": "Lubuk Sekam",
+    "tanggalLahir": "2007-06-14"
+  },
+  {
+    "nisn": "74696872",
+    "nama": "Habib Hadianta",
+    "tempatLahir": "Kuta Parit",
+    "tanggalLahir": "2007-05-15"
+  },
+  {
+    "nisn": "74691829",
+    "nama": "HASA DAUD",
+    "tempatLahir": "SELESAI",
+    "tanggalLahir": "2007-09-25"
+  },
+  {
+    "nisn": "79857143",
+    "nama": "Intan Mutiara",
+    "tempatLahir": "Kerpai",
+    "tanggalLahir": "2007-04-23"
+  },
+  {
+    "nisn": "75341614",
+    "nama": "LILY PRATIWI",
+    "tempatLahir": "KERPAI ",
+    "tanggalLahir": "2007-02-19"
+  },
+  {
+    "nisn": "72089180",
+    "nama": "Mehta Veronika Br Sitepu",
+    "tempatLahir": "Betengar",
+    "tanggalLahir": "2007-09-23"
+  },
+  {
+    "nisn": "77550670",
+    "nama": "MHD. DANIEL MA'ARIF",
+    "tempatLahir": "PADANG CERMIN",
+    "tanggalLahir": "2007-05-13"
+  },
+  {
+    "nisn": "75489384",
+    "nama": "Muhammad Iqbal Siregar",
+    "tempatLahir": "Selayang",
+    "tanggalLahir": "2007-12-04"
+  },
+  {
+    "nisn": "3077375834",
+    "nama": "NABILA SYAHRANI",
+    "tempatLahir": "SELAYANG BARU",
+    "tanggalLahir": "2007-05-22"
+  },
+  {
+    "nisn": "82405682",
+    "nama": "NUR UMMIYAH HASANAH",
+    "tempatLahir": "PERHIASAN ",
+    "tanggalLahir": "2008-02-02"
+  },
+  {
+    "nisn": "72674374",
+    "nama": "Octaria Bilqis Ramadani Br. Pasaribu",
+    "tempatLahir": "Sidomukti",
+    "tanggalLahir": "2007-10-05"
+  },
+  {
+    "nisn": "78037810",
+    "nama": "PENTI GADIS JUWITA",
+    "tempatLahir": "KEBUN LADA ",
+    "tanggalLahir": "2007-10-25"
+  },
+  {
+    "nisn": "73863066",
+    "nama": "Raditya Pramana",
+    "tempatLahir": "Lau Mulgap dsn 1",
+    "tanggalLahir": "2007-04-21"
+  },
+  {
+    "nisn": "83241168",
+    "nama": "RAIHANDA HAIKAL",
+    "tempatLahir": "TANJUNG KASIH",
+    "tanggalLahir": "2008-01-01"
+  },
+  {
+    "nisn": "76506454",
+    "nama": "RAJA PRATAMA",
+    "tempatLahir": "KAMPUNG BARU",
+    "tanggalLahir": "2007-11-07"
+  },
+  {
+    "nisn": "78789839",
+    "nama": "Rehan Alvaro Nasution",
+    "tempatLahir": "Ara Tunggal",
+    "tanggalLahir": "2007-07-27"
+  },
+  {
+    "nisn": "77702654",
+    "nama": "RIAN AFRIZA",
+    "tempatLahir": "LANGKAT",
+    "tanggalLahir": "2007-03-04"
+  },
+  {
+    "nisn": "73038347",
+    "nama": "Sela Aulia",
+    "tempatLahir": "Kerpai",
+    "tanggalLahir": "2007-04-30"
+  },
+  {
+    "nisn": "85659999",
+    "nama": "SINTIA FEBRIANI",
+    "tempatLahir": "BANDAR SAKTI",
+    "tanggalLahir": "2008-05-17"
+  },
+  {
+    "nisn": "74044127",
+    "nama": "SRI MAYANG",
+    "tempatLahir": "KERPAI ",
+    "tanggalLahir": "2007-08-01"
+  },
+  {
+    "nisn": "75482061",
+    "nama": "Yudisha Deloys Tarigan",
+    "tempatLahir": "Binjai",
+    "tanggalLahir": "2007-01-27"
+  },
+  {
+    "nisn": "72990119",
+    "nama": "Zahratun Janna",
+    "tempatLahir": "Binjai ",
+    "tanggalLahir": "2007-09-06"
+  },
+  {
+    "nisn": "79766713",
+    "nama": "ZIDAN FEBRIAN",
+    "tempatLahir": "MANCANG",
+    "tanggalLahir": "2006-10-08"
+  },
+  {
+    "nisn": "67615476",
+    "nama": "ZIKRIA ASYIFA",
+    "tempatLahir": "BATU BARA ",
+    "tanggalLahir": "2006-02-20"
+  },
+  {
+    "nisn": "79801322",
+    "nama": "ADAM DEPENDRA",
+    "tempatLahir": "SUKATANI",
+    "tanggalLahir": "2007-03-23"
+  },
+  {
+    "nisn": "78864688",
+    "nama": "AIDIL SYAHPUTRA BANGUN",
+    "tempatLahir": "MEDAN",
+    "tanggalLahir": "2007-04-15"
+  },
+  {
+    "nisn": "87701478",
+    "nama": "Aldo Revansyah",
+    "tempatLahir": "Lau Mulgap",
+    "tanggalLahir": "2008-04-01"
+  },
+  {
+    "nisn": "62983875",
+    "nama": "ARINDA NATASYA",
+    "tempatLahir": "SIKAPAS",
+    "tanggalLahir": "2006-12-29"
+  },
+  {
+    "nisn": "86712319",
+    "nama": "DEWI",
+    "tempatLahir": "PD.BRAHRANG",
+    "tanggalLahir": "2008-04-24"
+  },
+  {
+    "nisn": "71693003",
+    "nama": "dwi ayu anjani",
+    "tempatLahir": "muliorejo",
+    "tanggalLahir": "2007-09-16"
+  },
+  {
+    "nisn": "79382500",
+    "nama": "Elda Piana",
+    "tempatLahir": "AMAN DAMAI",
+    "tanggalLahir": "2007-01-25"
+  },
+  {
+    "nisn": "79393498",
+    "nama": "ERICA AULIA HASIBUAN",
+    "tempatLahir": "LAU MULGAP",
+    "tanggalLahir": "2007-06-15"
+  },
+  {
+    "nisn": "82672400",
+    "nama": "Feby Laurensia",
+    "tempatLahir": "Sidomukti",
+    "tanggalLahir": "2008-03-17"
+  },
+  {
+    "nisn": "72637788",
+    "nama": "Gladis Navira",
+    "tempatLahir": "Sidomukti",
+    "tanggalLahir": "2007-11-27"
+  },
+  {
+    "nisn": "75498212",
+    "nama": "ICA SANTIA",
+    "tempatLahir": "SEI TAMPAH",
+    "tanggalLahir": "2007-08-01"
+  },
+  {
+    "nisn": "66028205",
+    "nama": "KHAIRUN NISSA",
+    "tempatLahir": "Sidomulyo",
+    "tanggalLahir": "2006-11-24"
+  },
+  {
+    "nisn": "75229857",
+    "nama": "M.ANDIKA TAMBUNAN",
+    "tempatLahir": "BINJAI",
+    "tanggalLahir": "2007-10-20"
+  },
+  {
+    "nisn": "61657482",
+    "nama": "MAYAWI",
+    "tempatLahir": "SEI RAMPAH",
+    "tanggalLahir": "2006-07-14"
+  },
+  {
+    "nisn": "63461782",
+    "nama": "Melsyana",
+    "tempatLahir": "Sei Limbat",
+    "tanggalLahir": "2006-08-30"
+  },
+  {
+    "nisn": "3065779159",
+    "nama": "MHD. FADLI",
+    "tempatLahir": "BINJAI",
+    "tanggalLahir": "2006-01-13"
+  },
+  {
+    "nisn": "78175895",
+    "nama": "MHD.RIZKI AL HAFIZ",
+    "tempatLahir": "LAU MULGAP",
+    "tanggalLahir": "2007-06-19"
+  },
+  {
+    "nisn": "68492208",
+    "nama": "Muhammad Afriansyah",
+    "tempatLahir": "Aman Damai",
+    "tanggalLahir": "2006-04-10"
+  },
+  {
+    "nisn": "78471187",
+    "nama": "naysilla putri",
+    "tempatLahir": "LANGKAT",
+    "tanggalLahir": "2007-01-05"
+  },
+  {
+    "nisn": "72705340",
+    "nama": "Nike Enjelika",
+    "tempatLahir": "Bekulap",
+    "tanggalLahir": "2007-07-03"
+  },
+  {
+    "nisn": "77623160",
+    "nama": "PANDU RIZKI ALAMSYAH",
+    "tempatLahir": "DUMAI",
+    "tanggalLahir": "2007-05-18"
+  },
+  {
+    "nisn": "138519094",
+    "nama": "RADIT DWI ALFINO",
+    "tempatLahir": "SEI LIMBAT",
+    "tanggalLahir": "2007-02-28"
+  },
+  {
+    "nisn": "76052729",
+    "nama": "SANTIA BR GINTING",
+    "tempatLahir": "LAU MULGAP",
+    "tanggalLahir": "2007-03-29"
+  },
+  {
+    "nisn": "72887375",
+    "nama": "SINTA DWI ANGGRAINI",
+    "tempatLahir": "BINJAI",
+    "tanggalLahir": "2007-07-06"
+  },
+  {
+    "nisn": "78179094",
+    "nama": "Sintia Sara Depi",
+    "tempatLahir": "Medan",
+    "tanggalLahir": "2007-04-13"
+  },
+  {
+    "nisn": "72504073",
+    "nama": "Sri Reski",
+    "tempatLahir": "Pd.Brahrang",
+    "tanggalLahir": "2007-02-12"
+  },
+  {
+    "nisn": "76166943",
+    "nama": "SYAHBANI SALSABILA",
+    "tempatLahir": "PADANG CERMIN",
+    "tanggalLahir": "2007-09-08"
+  },
+  {
+    "nisn": "64337006",
+    "nama": "Syahrizal Lubis",
+    "tempatLahir": "Binjai",
+    "tanggalLahir": "2006-04-14"
+  },
+  {
+    "nisn": "68678553",
+    "nama": "Agustri Tamba",
+    "tempatLahir": "Berastagi",
+    "tanggalLahir": "2006-08-21"
+  },
+  {
+    "nisn": "78367337",
+    "nama": "ANGGA PRAMUDIANTORO",
+    "tempatLahir": "SUMBEREJO",
+    "tanggalLahir": "2007-06-30"
+  },
+  {
+    "nisn": "77052157",
+    "nama": "BELT SAZAR",
+    "tempatLahir": "SELAYANG",
+    "tanggalLahir": "2007-04-04"
+  },
+  {
+    "nisn": "86629769",
+    "nama": "Deni Ramadhan",
+    "tempatLahir": "Mancang",
+    "tanggalLahir": "2008-09-08"
+  },
+  {
+    "nisn": "76247604",
+    "nama": "DESTA PRANANTA TARIGAN",
+    "tempatLahir": "NAMU UKUR",
+    "tanggalLahir": "2007-12-25"
+  },
+  {
+    "nisn": "66064090",
+    "nama": "Dewi Kesuma",
+    "tempatLahir": "Cinta Dapat",
+    "tanggalLahir": "2006-08-08"
+  },
+  {
+    "nisn": "71340471",
+    "nama": "ELVIRA",
+    "tempatLahir": "SIDODADI",
+    "tanggalLahir": "2007-07-21"
+  },
+  {
+    "nisn": "66474142",
+    "nama": "FIKRIAN SAMUDRA",
+    "tempatLahir": "PERHIASAN",
+    "tanggalLahir": "2006-06-03"
+  },
+  {
+    "nisn": "73110737",
+    "nama": "GITA AULIA PUTRI",
+    "tempatLahir": "CINTA DAPAT",
+    "tanggalLahir": "2007-09-09"
+  },
+  {
+    "nisn": "76695932",
+    "nama": "ISNAINI ZAHRA",
+    "tempatLahir": "MEDAN",
+    "tanggalLahir": "2007-03-26"
+  },
+  {
+    "nisn": "44154590",
+    "nama": "JUNIARTA Br HUTASOIT",
+    "tempatLahir": "TALANG LAKAT",
+    "tanggalLahir": "2004-06-05"
+  },
+  {
+    "nisn": "77462064",
+    "nama": "KEVIN HIDAYAT",
+    "tempatLahir": "LANGKAT",
+    "tanggalLahir": "2007-01-09"
+  },
+  {
+    "nisn": "75861429",
+    "nama": "M. ZIDAN FAHRUQI",
+    "tempatLahir": "LAU MULGAP",
+    "tanggalLahir": "2007-09-06"
+  },
+  {
+    "nisn": "84545782",
+    "nama": "Mhd.Ridho I",
+    "tempatLahir": "Sumber Rejo",
+    "tanggalLahir": "2008-02-18"
+  },
+  {
+    "nisn": "93182799",
+    "nama": "Muhammad Fadil",
+    "tempatLahir": "Perhiasan",
+    "tanggalLahir": "2009-03-23"
+  },
+  {
+    "nisn": "51678655",
+    "nama": "MUTIARA NJILEKENSA BR GINTING",
+    "tempatLahir": "SUKA RAME",
+    "tanggalLahir": "2005-01-07"
+  },
+  {
+    "nisn": "77129649",
+    "nama": "NADIYA SAFIRA",
+    "tempatLahir": "PERHIASAN",
+    "tanggalLahir": "2007-04-21"
+  },
+  {
+    "nisn": "78502288",
+    "nama": "NOVITA",
+    "tempatLahir": "SEI LIMBAT",
+    "tanggalLahir": "2007-04-19"
+  },
+  {
+    "nisn": "88978590",
+    "nama": "PANJI AL AYUBBI",
+    "tempatLahir": "BINJAI",
+    "tanggalLahir": "2008-01-11"
+  },
+  {
+    "nisn": "62163241",
+    "nama": "Putri Aulia Pratiwi",
+    "tempatLahir": "Binjai",
+    "tanggalLahir": "2006-05-22"
+  },
+  {
+    "nisn": "89973486",
+    "nama": "Rendy Prasetia",
+    "tempatLahir": "Lau Mulgap",
+    "tanggalLahir": "2008-01-15"
+  },
+  {
+    "nisn": "77183265",
+    "nama": "RINI ADELIA",
+    "tempatLahir": "TANJUNG JATI",
+    "tanggalLahir": "2007-09-09"
+  },
+  {
+    "nisn": "79522799",
+    "nama": "RISKA AMELIA",
+    "tempatLahir": "TANJUNG JATI",
+    "tanggalLahir": "2007-06-04"
+  },
+  {
+    "nisn": "46931206",
+    "nama": "SANDI SIAHAAN",
+    "tempatLahir": "BATAS",
+    "tanggalLahir": "2004-06-17"
+  },
+  {
+    "nisn": "75205051",
+    "nama": "STEPANUS RANTE SITEPU",
+    "tempatLahir": "PERHIASAN",
+    "tanggalLahir": "2008-04-19"
+  },
+  {
+    "nisn": "78121245",
+    "nama": "Sufriyanti Br Napitupulu",
+    "tempatLahir": "Selayang",
+    "tanggalLahir": "2007-08-10"
+  },
+  {
+    "nisn": "56484860",
+    "nama": "SUHADA",
+    "tempatLahir": "SUMBER REJO",
+    "tanggalLahir": "2007-05-29"
+  },
+  {
+    "nisn": "77750007",
+    "nama": "TANIA AUREL",
+    "tempatLahir": "BINJAI",
+    "tanggalLahir": "2007-08-08"
+  },
+  {
+    "nisn": "72832172",
+    "nama": "WAHYU SRI NINGSIH BR SIHOTANG",
+    "tempatLahir": "UKUI",
+    "tanggalLahir": "2007-11-28"
+  },
+  {
+    "nisn": "83619727",
+    "nama": "Winata Fraga Tarigan",
+    "tempatLahir": "Binjai",
+    "tanggalLahir": "2008-01-17"
+  },
+  {
+    "nisn": "88103073",
+    "nama": "Winata Fraja Tarigan",
+    "tempatLahir": "Binjai",
+    "tanggalLahir": "2008-01-17"
+  },
+  {
+    "nisn": "76434568",
+    "nama": "Yusuf Fransisco Situmorang",
+    "tempatLahir": "Selayang",
+    "tanggalLahir": "2007-07-14"
+  },
+  {
+    "nisn": "79360023",
+    "nama": "Zahra Varissya AK",
+    "tempatLahir": "Langsa",
+    "tanggalLahir": "2007-09-17"
+  }
+];
