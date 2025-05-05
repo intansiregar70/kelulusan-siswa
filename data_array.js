877d39c7858581524f1ed25d147799e2964146f6
intansiregar70 <intansiregar79@admin.sma.belajar.id>
2025-05-05 11:15:45 +0700

    Add files via upload

---
+++ data_array.js
@@ -0,0 +1,104 @@
+const data = [
+  {
+    "nisn": "0073497717",
+    "nama": "ALPAN AGUSTINUS SIHOMBING",
+    "tempatLahir": "Binjai",
+    "tanggalLahir": "2007-08-18"
+  },
+  {
+    "nisn": "0072115257",
+    "nama": "ANNISA MALIKA OLIVIA BR. GINTING",
+    "tempatLahir": "Binjai",
+    "tanggalLahir": "2007-12-15"
+  },
+  {
+    "nisn": "0078541316",
+    "nama": "AULIA SYAHFITRI",
+    "tempatLahir": "Binjai",
+    "tanggalLahir": "2007-01-06"
+  },
+  {
+    "nisn": "0077146709",
+    "nama": "BAGUS PRABOWO ANANTA",
+    "tempatLahir": "Binjai",
+    "tanggalLahir": "2007-10-01"
+  },
+  {
+    "nisn": "0077156020",
+    "nama": "BINTANG SYAFRIEL SETIAWAN",
+    "tempatLahir": "Medan",
+    "tanggalLahir": "2007-04-04"
+  },
+  {
+    "nisn": "0073088137",
+    "nama": "CESYA AZIRA HARAHAP",
+    "tempatLahir": "Binjai",
+    "tanggalLahir": "2007-11-17"
+  },
+  {
+    "nisn": "0074387540",
+    "nama": "CHAILA ALYSA PUTRI BR SIREGAR",
+    "tempatLahir": "Air Tenang",
+    "tanggalLahir": "2007-05-22"
+  },
+  {
+    "nisn": "0076363080",
+    "nama": "CUT MUTIYA IZMI",
+    "tempatLahir": "Binjai",
+    "tanggalLahir": "2007-03-01"
+  },
+  {
+    "nisn": "0075713897",
+    "nama": "DAFFA LIDYAN SIREGAR",
+    "tempatLahir": "Binjai",
+    "tanggalLahir": "2007-08-24"
+  },
+  {
+    "nisn": "0076895457",
+    "nama": "DICKY RIZMADHANI",
+    "tempatLahir": "Binjai",
+    "tanggalLahir": "2007-09-18"
+  },
+  {
+    "nisn": "0077737631",
+    "nama": "DIVA OKTAVIANA SIAHAAN",
+    "tempatLahir": "P. Brandan",
+    "tanggalLahir": "2007-10-29"
+  },
+  {
+    "nisn": "0072352599",
+    "nama": "GIRADO STEVEN ROLANSKY PURBA",
+    "tempatLahir": "Binjai",
+    "tanggalLahir": "2007-09-25"
+  },
+  {
+    "nisn": "0062440522",
+    "nama": "HANI",
+    "tempatLahir": "Binjai",
+    "tanggalLahir": "2006-11-10"
+  },
+  {
+    "nisn": "0076945126",
+    "nama": "IMAM DZAKI HABIBI NASUTION",
+    "tempatLahir": "Binjai",
+    "tanggalLahir": "2007-05-27"
+  },
+  {
+    "nisn": "0086552269",
+    "nama": "IZA FADRI",
+    "tempatLahir": "Telaga Jernih",
+    "tanggalLahir": "2008-11-21"
+  },
+  {
+    "nisn": "0079487231",
+    "nama": "KEVIN PRANSISKO MANURUNG",
+    "tempatLahir": "Binjai",
+    "tanggalLahir": "2007-06-04"
+  },
+  {
+    "nisn": "0079896776",
+    "nama": "ZONA TRISON SIAHAAN",
+    "tempatLahir": "Binjai",
+    "tanggalLahir": "2007-12-22"
+  }
+];
